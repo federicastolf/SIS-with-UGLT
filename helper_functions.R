989615a1d4378686d39904beff2f7cecaab68ae1
federicastolf <federica.stolf@gmail.com>
2025-11-17 16:42:08 -0500

    update simulations block

--- helper_functions.R
+++ helper_functions.R
@@ -1,7 +1,8 @@
 library(MASS)
 
-# simulate data with bloc-structure covariance
-simulate_block_mvn <- function(n, p, n_blocks, within_cor, between_cor, mseed) {
+# simulate data with block structure covariance
+simulate_block_mvn <- function(n, p, n_blocks, within_cor, between_cor, variances, 
+                               mseed) {
   set.seed(mseed)
   block_size <- p / n_blocks
   # Allow different correlations for each block
@@ -24,11 +25,31 @@ simulate_block_mvn <- function(n, p, n_blocks, within_cor, between_cor, mseed) {
     diag(Sigma[start_idx:end_idx, start_idx:end_idx]) <- 1
   }
   
+  D <- diag(rep(sqrt(variances), p))
+  Sigma1 <- D %*% Sigma %*% D
   # Simulate data
   Y <- mvrnorm(n = n, mu = rep(0, p), Sigma = Sigma)
+  xlab <- as.factor(rep(1:n_blocks, each = block_size))
   
-  return(list(data = Y, Sigma = Sigma))
+  return(list(data = Y, Sigma = Sigma1, xlab = xlab))
 }
+
+reorder_blocks <- function(Sigma, Y, block_membership, new_block_order) {
+  p <- ncol(Y)
+  n_blocks <- length(unique(block_membership))
+  row_order <- unlist(lapply(new_block_order, function(b) {
+    which(block_membership == b)
+  }))
+  Sigma_reordered <- Sigma[row_order, row_order]
+  Y_reordered <- Y[, row_order]
+  block_membership_reordered <- block_membership[row_order]
+  
+  return(list(Sigma = Sigma_reordered,
+              Y = Y_reordered, xlab = block_membership_reordered, 
+              row_order = row_order, new_block_order = new_block_order
+  ))
+}
+
 
 # Covariance decreases with index
 simulate_decreasing_cov <- function(p, x = NULL, max_var, min_var, max_cor, 
@@ -162,80 +183,6 @@ identify_lambda <- function(fit, p, nsample = NULL,
   
   return(result)
 }
-
-
-
-#------------------------------------------------------------------------------#
-# # function to simulate synthetic data
-# simulate_data = function(n, p, k, covariate, sd_gamma, cp, sigma_sq, mseed){
-#   set.seed(mseed)
-#   q = dim(covariate)[2]
-#   gamma = matrix(rnorm(q*p, sd = sd_gamma), q,p) # regression coefficient
-#   pilogit = cp*plogis(covariate %*% gamma) 
-#   get_phi = simPhi(p, k, pilogit)
-#   Phi = get_phi$Phi
-#   theta_inv = rgamma(k,1,1)
-#   Lambda = matrix(0,p,k)
-#   for(j in 1:p){
-#     for(h in 1:k){
-#       Lambda[j,h]= Phi[j,h]/theta_inv[h]
-#     }
-#   }
-#   M = matrix(rnorm(n*k, 0, 1), ncol=k) # factor scores
-#   # data
-#   Y = M %*% t(Lambda) + sqrt(sigma_sq)*matrix(rnorm(n*p), nrow = n, ncol = p) 
-#   return(list("Y" = Y, "Lambda" = Lambda, "Phi"=Phi, "pivot"=get_phi$pivot,
-#               "delta"=get_phi$Delta, "gamma"=gamma))
-# }
-# 
-# # function for simulate Phi via gibbs-sampler
-# simPhi = function(p, k, pilogit, NiterGibbs = 200){
-#   # Initialization 
-#   phi = matrix(rbinom(p*k, 1, 0.2), p, k) 
-#   delta = phi
-#   l=rep(NA, k) # pivots
-#   l[1] = which(phi[,1]>0)[1]
-#   for(h in 2:k){
-#     w = which(phi[,h]>0) # counting 1's in the h column of Delta
-#     l[h] = w[!w %in% l[-h]][1] # take the first
-#     if(is.na(l[h])){
-#       delta[,h] = 0 # put all column to zeros
-#     }else{
-#       delta[1:l[h],h] = 0 # before pivots all zeros
-#       delta[l[h]:p, h] =  phi[l[h]:p,h] # from pivot and after equal to phi[,l]
-#     }
-#   }
-#   
-#   for(t in 2:NiterGibbs){
-#     for(h in 1:k){ 
-#       Lh = sort(l[-h])
-#       p_h = pilogit[,h]
-#       k = 0     
-#       # definisco prob di phi_h condizionatamente agli altri pivot
-#       for(pl in Lh){
-#         k = k+1
-#         if (pl-k>0){
-#           scale = 1-prod(1-pilogit[1:pl,h][-Lh[1:k]])
-#           p_h[pl] = min(p_h[pl]/scale, 1)
-#         }
-#         else{
-#           p_h[pl] = 1
-#         }
-#       }
-#       phi[,h] = rbinom(p,1,p_h) # generate phi
-#       w = which(phi[,h]>0)
-#       l[h] = w[!w %in% l[-h]][1]
-#       if(is.na(l[h])){
-#         delta[,h] = 0
-#       }else{
-#         delta[1:l[h],h] = 0
-#         delta[l[h]:p, h] =  phi[l[h]:p,h]
-#       }
-#     }
-#   }
-#   # return the last phi and delta
-#   return(list("Phi" = phi, "Delta"= delta, "pivot"=l))
-# }
 
 
 # function to reorder and sign swap based on delta and lambda
---
+++ simulationsBlock.R
@@ -0,0 +1,127 @@
+library(pheatmap)
+library(gridExtra)
+library(sparvaride)
+library(tidyverse)
+
+rm(list=ls())
+
+source("gibbs.R")
+source("helper_functions.R")
+
+#----------------------# scenario 1 : 3 block covariance #------------------------#
+
+# simulate data
+n = 50
+p = 30
+mseed = 435
+data_synt <- simulate_block_mvn(n, p, n_blocks = 3, within_cor = c(0.4, 0.9, 0.6),
+                                between_cor = 0,variances = 1, mseed)
+# data_synt <- simulate_block_mvn(n, p, n_blocks = 5, within_cor = c(0.5, 0.8, 0.7, 0.9, 0.6),
+#                                 between_cor = 0,variances = 1, mseed)
+
+Y <- data_synt$data
+covariate = model.matrix(~data_synt$xlab) # X matrix
+
+# gibbs parameters
+nrun = 8000
+burn = round(nrun/2)
+thin = 1
+# adaptation parameters
+b0 = 1
+b1 = 5 * 10^(-4)
+start_adapt = 100
+# hyperparameters 
+alpha = 5 # param CUSP
+a_sigma = 1
+b_sigma = 1
+a_theta = 1
+b_theta = 1
+sd_gammaB = 1 # Standard deviation for \eqn{\gamma_{hB}} prior distribution.
+# p_constant = log(p)/p*2*exp(1)
+# p_constant = 0.1
+p_constant = 0.05
+scale_factor_MH = 1
+cMH = 0.001
+
+fit = gibbs_adaptive(Y, covariate, nrun, burn, thin, mseed, verbose = T, p_constant,
+                     b0, b1, start_adapt, alpha, a_sigma, b_sigma, a_theta,
+                     b_theta, sd_gammaB, scale_factor_MH, cMH)
+
+# ---- Covariance ----
+Lambda_outer <- lapply(fit$lambda, function(A) A %*% t(A))
+cov_mean = apply(simplify2array(Lambda_outer), c(1, 2), mean)
+
+
+max_val <- max(data_synt$Sigma)
+p3 = pheatmap(data_synt$Sigma, cluster_rows = F, cluster_cols = F, 
+              border_color ="NA",   main = "True covariance",
+              breaks = seq(0, max_val, length.out = 100),
+              color = colorRampPalette(c("white","yellow2", "orange","darkred"))(100))
+
+p1 = pheatmap(cov_mean, treeheight_row = 0, treeheight_col = 0, cluster_rows = F,
+              cluster_cols = F, border_color ="NA", legend=T,
+              main = "Posterior covariance",
+              breaks = seq(0, max_val, length.out = 100),
+              color = colorRampPalette(c("white","yellow2", "orange","darkred"))(100))
+
+pblock3 = grid.arrange(p3[[4]], p1[[4]], nrow = 1)
+# ggsave("combined_heatmaps.pdf", pblock3, width = 12, height = 6)
+
+#---- Inference on loadings ----
+
+ppLambda <- identify_lambda(fit, p)
+Lambda_mean = apply(simplify2array(ppLambda$lambda), c(1, 2), mean)
+
+pp1 = pheatmap(Lambda_mean, treeheight_row = 0, treeheight_col = 0, cluster_rows = F,
+               cluster_cols = F, border_color ="NA", legend=T,
+               main="Lambda",
+               breaks = seq(min(Lambda_mean), max(Lambda_mean), length.out = 100),
+               color = colorRampPalette(c("white","gold2", "orange","darkred"))(100))
+
+ggsave("Lambda.pdf", pp1 , width = 12, height = 6)
+
+#-------------# reorder blocks #---------#
+
+data_reordered <- reorder_blocks(Sigma = data_synt$Sigma, Y = Y,
+                            block_membership = data_synt$xlab,
+                            new_block_order = c(3, 1, 2))
+# data_reordered <- reorder_blocks(Sigma = data_synt$Sigma, Y = Y, 
+#                                  block_membership = data_synt$xlab, 
+#                                  new_block_order = c(5,4,3, 1, 2))
+
+Yr <- data_reordered$Y
+covariater = model.matrix(~data_reordered$xlab) # X matrix
+fit_r = gibbs_adaptive(Yr, covariater, nrun, burn, thin, mseed, verbose = T, p_constant,
+                     b0, b1, start_adapt, alpha, a_sigma, b_sigma, a_theta,
+                     b_theta, sd_gammaB, scale_factor_MH, cMH)
+
+#--# Covariance 
+Lambda_outerr <- lapply(fit_r$lambda, function(A) A %*% t(A))
+cov_meanr = apply(simplify2array(Lambda_outerr), c(1, 2), mean)
+
+
+
+pr3 = pheatmap(data_reordered$Sigma, cluster_rows = F, cluster_cols = F, 
+              border_color ="NA",   main = "True covariance- reordered",
+              breaks = seq(0, max_val, length.out = 100),
+              color = colorRampPalette(c("white","yellow2", "orange","darkred"))(100))
+pr1 = pheatmap(cov_meanr, treeheight_row = 0, treeheight_col = 0, cluster_rows = F,
+              cluster_cols = F, border_color ="NA", legend=T,
+              main = "Posterior covariance-reordered",
+              breaks = seq(0, max_val, length.out = 100),
+              color = colorRampPalette(c("white","yellow2", "orange","darkred"))(100))
+
+prblock3 = grid.arrange(pr3[[4]], pr1[[4]], nrow = 1)
+# ggsave("reordere_heatmaps.pdf", prblock3, width = 12, height = 6)
+
+
+#----# Lambda
+ppLambdar <- identify_lambda(fit_r, p)
+Lambda_meanr = apply(simplify2array(ppLambdar$lambda), c(1, 2), mean)
+
+ppr1 = pheatmap(Lambda_mean, treeheight_row = 0, treeheight_col = 0, cluster_rows = F,
+               cluster_cols = F, border_color ="NA", legend=T,
+               main="Lambda",
+               breaks = seq(min(Lambda_mean), max(Lambda_mean), length.out = 100),
+               color = colorRampPalette(c("white","gold2", "orange","darkred"))(100))
+
